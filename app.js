41f655fdcdbb79838218424cea68d1a3ee501997
devyohan <ticonweb@gmail.com>
2018-12-29 18:20:40 +0900

    routes 정리

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
+import routers from './routes';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
+import routes from './routes';
 
 const app = express();
 
@@ -18,8 +20,8 @@ app.use(helmet());
 
 // middleware 는 next 대신 res.send 로 전송시 연결이 끊어집니다.
 
-app.use('/', globalRouter);
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
