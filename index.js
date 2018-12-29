3f5b0347f898893e9cc6f17dc9bbf8fc65236c98
devyohan <ticonweb@gmail.com>
2018-12-29 15:08:36 +0900

    cookie, body parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 import express from 'express';
 import logger from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
 const app = express();
 const PORT = 4000;
 
@@ -11,10 +15,17 @@ const handleListening = () =>
 
 const handlProfile = (req, res) => res.send("Profile!!");
 
+// app.use 는 순서를 지키며 실행해주세요. 
+
 // tiny : GET / 304 - - 3.842 ms
 // app.use(logger('tiny'));
 // dev : GET / 304 4.104 ms - -
+app.use(cookieParser());
+app.use(bodyParser());
 app.use(logger('dev'));
+app.use(helmet());
+
+// middleware 는 next 대신 res.send 로 전송시 연결이 끊어집니다.
 
 app.get('/', handlProfile);
 
