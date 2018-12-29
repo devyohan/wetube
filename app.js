7a4d393fc3c38f7f3756d80f90d7fbeeb620a791
devyohan <ticonweb@gmail.com>
2018-12-29 15:36:26 +0900

    globalRouter 추가
    메인 페이지, 검색 등 router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
+import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 
@@ -17,6 +18,7 @@ app.use(helmet());
 
 // middleware 는 next 대신 res.send 로 전송시 연결이 끊어집니다.
 
+app.use('/', globalRouter);
 app.use('/user', userRouter);
 app.use('/video', videoRouter);
 
