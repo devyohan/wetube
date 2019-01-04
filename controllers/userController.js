8c4f36a69175bbb5263d8b4e1f54679e4245387e
devyohan <ticonweb@gmail.com>
2019-01-04 17:05:46 +0900

    더미 데이터로 로그인 여부에 따른 페이지 표시

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,32 @@
-export const join = (req, res) => res.render('join', { pageTitle: 'Join' });
-export const login = (req, res) => res.render('login', { pageTitle: 'Login' });
-export const logout = (req, res) => res.send('logout');
-export const userDetail = (req, res) => res.render('userDetail', { pageTitle: 'UserDetail' });
-export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'EditProfile' });
-export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'Change Password' });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    res.render('join', { pageTitle: 'Join' });
+};
+
+export const postJoin = (req, res) => {
+    const {
+        body: { name, email, password, passwordverify }
+    } = req;
+    if(password !== passwordverify) {
+        res.status(400);
+        res.render('join', { pageTitle: 'Join' });
+    } else {
+        // To Do: Register user
+        res.redirect(routes.home);
+    }
+};
+
+export const getLogin = (req, res) =>
+    res.render('login', { pageTitle: 'Login' });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+export const logout = (req, res) =>
+    res.send('logout');
+export const userDetail = (req, res) =>
+    res.render('userDetail', { pageTitle: 'UserDetail' });
+export const editProfile = (req, res) =>
+    res.render('editProfile', { pageTitle: 'EditProfile' });
+export const changePassword = (req, res) =>
+    res.render('changePassword', { pageTitle: 'Change Password' });
