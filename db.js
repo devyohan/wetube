38ab17644865e87a655f5e5b756a006c378094f8
devyohan <ticonweb@gmail.com>
2019-01-04 11:35:08 +0900

    Dummy dataset

---
+++ db.js
@@ -0,0 +1,39 @@
+// Fake DB
+export const videos = [
+    {
+        id: 100000,
+        title: 'Video AWESOME!!',
+        description: 'This is something that Awesome.',
+        views: 22,
+        videoFile: 'https://archive.org/download/BigBuckBunny_328/BigBuckBunny_512kb.mp4',
+        creator: {
+            id: 112211,
+            name: 'Yohan Park',
+            email: 'asdf@qwer.com'
+        }
+    },
+    {
+        id: 100001,
+        title: 'Video QWER!!',
+        description: 'This is QWER.',
+        views: 22,
+        videoFile: 'https://archive.org/download/BigBuckBunny_328/BigBuckBunny_512kb.mp4',
+        creator: {
+            id: 112211,
+            name: 'Yohan Park',
+            email: 'asdf@qwer.com'
+        }
+    },
+    {
+        id: 100002,
+        title: 'Video ZXCV!!',
+        description: 'This is ZXCV.',
+        views: 22,
+        videoFile: 'https://archive.org/download/BigBuckBunny_328/BigBuckBunny_512kb.mp4',
+        creator: {
+            id: 112211,
+            name: 'Yohan Park',
+            email: 'asdf@qwer.com'
+        }
+    }
+];
